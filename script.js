0113287b84e755a0cee7db1c8dafa58c8c079500
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-07-19 15:57:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // complete this js code
-function Person(name, age) {}
-
-function Employee(name, age, jobTitle) {}
-
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
+Person.prototype.greet = function(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+}
+Employee.prototype.__proto__ = Person.prototype;
+function Employee(name, age, jobTitle) {
+	Person.call(this, name, age);
+    this.jobTitle = jobTitle; 
+}
+Employee.prototype.jobGreet = function() {
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
